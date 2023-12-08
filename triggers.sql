c555a083ad4564f8cf970d250ea538ac5ef601ca
Agustin Durand <agustindurand.98@gmail.com>
2023-12-08 19:20:46 -0300

    triggers

---
+++ triggers.sql
@@ -0,0 +1,43 @@
+
+create table expenses_log (
+id int primary key auto_increment,
+fecha date,
+expenseId int
+);
+
+Create trigger control after update on army_corps_expenses
+for each row
+insert into expenses_log (fecha, expenseId) values (now(),new.id);   
+
+
+
+
+
+
+DELIMITER //
+
+CREATE TRIGGER audit_log_soldiers_quantity AFTER INSERT ON soldiers_quantity
+FOR EACH ROW
+BEGIN
+    INSERT INTO soldiers_quantity_audit (fecha, accion, usuario) VALUES (NOW(), 'INSERT', USER());
+END;
+//
+
+DELIMITER ;
+
+
+
+
+
+
+
+DELIMITER //
+
+CREATE TRIGGER delete_soldiers_company AFTER DELETE ON soldiers_quantity
+FOR EACH ROW
+BEGIN
+    DELETE FROM company WHERE Id_Company = OLD.id_company;
+END;
+//
+
+DELIMITER ;
